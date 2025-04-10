70553f8f9597446a6692ba8a5534c74853e92e2a
Denis Roussel <droussel@centreon.com>
2025-04-10 07:42:56 -0400

    Add additional informations on plot (#2)

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from nixtla import NixtlaClient
 nixtla_client = NixtlaClient()
 nixtla_client.validate_api_key()
 
+TICKER = "BTC-USD"
+
 
 def get_data(
-    ticker: str = "BTC-USD",
+    ticker: str = TICKER,
     end: datetime.date = datetime.date.today(),
     period: datetime.timedelta = datetime.timedelta(days=180),
 ) -> pd.DataFrame:
@@ -33,7 +35,17 @@ def predict(data: pd.DataFrame, h: int = 30, freq: str = "D") -> pd.DataFrame:
 def save_predictions(
     historic: pd.DataFrame, predictions: pd.DataFrame, output: str = "plot.png"
 ) -> None:
-    fig = nixtla_client.plot(historic, predictions)
+    current_date = datetime.date.today().strftime("%Y-%m-%d")
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    nixtla_client.plot(historic, predictions, ax=ax)
+    ax.set_title(f"Predictions of {TICKER} as of {current_date}")
+    ax.set_xlabel("")  # Remove x-axis title
+    ax.set_ylabel("")  # Remove y-axis title
+    ax.get_yaxis().set_major_formatter(
+        plt.FuncFormatter(lambda x, _: f"${x:,.0f}")
+    )  # Format y-axis with $ and pretty print
     fig.savefig(output)
 
 
